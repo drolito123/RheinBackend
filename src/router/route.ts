c0f73768ed7435900ba4c513b0c12cce5eb36814
Pensado Berbert Rolim Pedro <p.pensado@etec.local>
2023-10-25 13:06:21 -0300

    hasta punto 2

--- src/router/route.ts
+++ src/router/route.ts
@@ -5,4 +5,15 @@ rout.get('/', (_, res) => {
     res.send("donde hubo fuego cenizas quedan");
 });
 
+const pym = [
+    {nombre: "samsung", modelo: "x10", precio: 1100, pais: "corea"},
+    {nombre: "iphone", modelo: "p2p", precio: 900, pais: "usa"},
+    {nombre: "motorola", modelo: "h2r2", precio: 90, pais: "usa"}
+];
+
+rout.get('/ny', (_, res) => {
+    res.send(pym.filter((prod) => prod.precio > 100));
+});
+
+
 export default rout;
