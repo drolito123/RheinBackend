8fea5f3b4b35b0f73e1e4b4dd3253643d4f6ec88
Pensado Berbert Rolim Pedro <p.pensado@etec.local>
2023-11-03 13:00:04 -0300

    falta el 5 y el 6

--- src/router/route.ts
+++ src/router/route.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const rout = express();
+const rout = express.Router();
 rout.get('/', (_, res) => {
     res.send("donde hubo fuego cenizas quedan");
 });
@@ -15,5 +15,54 @@ rout.get('/ny', (_, res) => {
     res.send(pym.filter((prod) => prod.precio > 100));
 });
 
+rout.post('/ny', (req, res) => {
+    const newProduct = req.body;
+    pym.push(newProduct);
+    res.status(201).json(newProduct);
+});
+
+rout.put('/pym/:nombre', (req, res) => {
+    const nombre = req.params.nombre;
+    const { newname, newmodel, newprice, newcountry } = req.body;
+
+    const i = pym.findIndex(product => product.nombre === nombre);
+    
+    if (i !== -1) {
+        pym[i].nombre = newname;
+        pym[i].modelo = newmodel;
+        pym[i].precio = newprice;
+        pym[i].pais = newcountry;
+      
+        res.send('Producto actualizado correctamente');
+    } else {
+        res.send('Producto no encontrado');
+    }
+});
+
+rout.put('/pym/:modelo', (req, res) => {
+    const model = req.params.modelo;
+
+    const i = pym.findIndex(modelo => modelo.nombre === model);
+    
+    if (i !== -1) {
+        pym.splice(i, 1)
+        res.send('Producto Eliminado');
+    } else {
+        res.send('Producto no encontrado');
+    }
+});
+
+rout.put('/pym/precio/:precio', (req, res) => {
+    const precio = req.params.precio;
+
+    const i = pym.findIndex(price => price.nombre === precio);
+    
+    if (i !== -1) {
+        pym.splice(i, 1)
+        res.send('Producto Eliminado');
+    } else {
+        res.send('Producto no encontrado');
+    }
+});
 
 export default rout;
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import rout from "./router/route";
 
 const app = express();
 const port = 8080;
+app.use(express.json());
 app.use("/", rout);
 
 app.listen(port, () =>{
