0f526bd24d0e5bd70e610e34677d76e043de78f4
Pensado Berbert Rolim Pedro <p.pensado@etec.local>
2023-11-10 13:05:48 -0300

    falta archivo controler

--- src/controlers/controler.ts
+++ src/controlers/controler.ts
@@ -20,3 +20,37 @@ export function SetNewProduct (req: Request, res: Response) {
     pym.push(newProduct);
     res.status(201).json(newProduct);
 };
+
+export function ModifyProduct (req: Request, res: Response) {
+    const nombre = req.params.nombre;
+    const { newname, newmodel, newprice, newcountry } = req.body;
+
+    const i = pym.findIndex(product => product.nombre === nombre);
+    
+    if (i !== -1) {
+        pym[i].nombre = newname;
+        pym[i].modelo = newmodel;
+        pym[i].precio = newprice;
+        pym[i].pais = newcountry;
+      
+        res.send('Producto actualizado correctamente');
+    } else {
+        res.send('Producto no encontrado');
+    }
+};
+
+export function getProductsByModels (req: Request, res: Response) {
+    const modelo = req.params.modelo;
+    
+    const actualizarProducto = req.body;
+
+    const index = pym.findIndex((product) => product.modelo === modelo);
+  
+    if (index !== -1) {
+      pym[index] = actualizarProducto;
+      res.json(pym[index]);
+    } else {
+      res.status(404).json({ message: "Producto no encontrado" });
+    }
+  };
+
